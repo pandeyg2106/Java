9c6944ec0482d87fdc78dcd33ccd48080f8bec6a
Gaurav <pandeyg2106@gmail.com>
2017-09-15 17:22:00 -0400

    Updated the perfect number program

--- Java Assignment/src/chapter5/Question33.java
+++ Java Assignment/src/chapter5/Question33.java
@@ -5,6 +5,7 @@ public class Question33 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int num=2, count=0;
+		System.out.println("The first 4 perfect numbers are: \n");
 		while (num<10000){
 			int sum=0;
 			for(int i=1;i<=num/2;i++){
@@ -14,7 +15,7 @@ public class Question33 {
 			}
 			if (sum==num){
 				count+=1;
-				System.out.print(+count+"th perfect number is "+num+"\n");
+				System.out.print(+num+"\n");
 			}
 			num+=1;
 		}
